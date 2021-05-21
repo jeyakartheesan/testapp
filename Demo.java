d489e3b359d707e3af4bd7e7ba2b2544dd6efd33
jeya kartheesan <51900929+jeyakartheesan@users.noreply.github.com>
2021-05-21 11:53:41 +0530

    Update Demo.java
    jk first commit

--- Demo.java
+++ Demo.java
@@ -2,7 +2,6 @@ class Demo
 {
     public static void main(String[] args) {
         System.out.println("Jeya kartheesan");
-        System.out.print("mathan kumar");
-        System.out.print("JK");
+
     }
 }
